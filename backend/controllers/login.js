924cdfe87bd9cf3ffb6d2328f42577c66df29a81
Sergey Golosnoy <sergeygolosnoy@gmail.com>
2023-10-07 17:34:27 +0300

    Update login.js

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -8,7 +8,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // eslint-disable-next-line no-unused-vars
-      const token = jwt.sign({ _id: user._id }, 'secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
       res.status(200).send({
         message: 'Авторизация прошла успешно',
         token,
