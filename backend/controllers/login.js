eac2b3a4f18717e199cd618363b2e7bf71618f03
Sergey Golosnoy <sergeygolosnoy@gmail.com>
2023-10-08 12:37:06 +0300

    fix env

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // eslint-disable-next-line no-unused-vars
-      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.status(200).send({
         message: 'Авторизация прошла успешно',
         token,
